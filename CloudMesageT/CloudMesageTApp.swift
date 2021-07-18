4c5e7b9f610db7e7dea1f1742a0b435f9e808f1b
Elechim01 <68447158+Elechim01@users.noreply.github.com>
2021-07-18 19:13:04 +0200

    Initial Commit

---
+++ CloudMesageT/CloudMesageTApp.swift
@@ -0,0 +1,17 @@
+//
+//  CloudMesageTApp.swift
+//  CloudMesageT
+//
+//  Created by Michele Manniello on 18/07/21.
+//
+
+import SwiftUI
+
+@main
+struct CloudMesageTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
